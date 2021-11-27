912fcdad4352e51531e1956a586737a107c3a4f7
Arne Groskurth <arne.groskurth@milchundzucker.de>
2021-11-27 17:01:50 +0100

    Simplification

--- src/tools/SliceScrollingTool.js
+++ src/tools/SliceScrollingTool.js
@@ -10,11 +10,6 @@ export class SliceScrollingTool extends Tool {
     canvasElement.addEventListener('wheel', SliceScrollingTool.#wheelEventHandler);
   }
 
-  deactivate(canvasElement) {
-
-    canvasElement.removeEventListener('wheel', SliceScrollingTool.#wheelEventHandler);
-  }
-
   static #wheelEventHandler(event) {
 
     const canvasElement = event.target;
