b4d7b2333b9f9c6ff32c9213db036186d60eceb2
Fionn Keogh <f.maceochaidh@web.de>
2021-12-09 19:22:04 +0100

    removed comments

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,25 +1,3 @@
-// const { spawnSync } = require('child_process');
-// const path = require('path');
-// const puppeteer = require('puppeteer');
-
-// const BROWSER_PATH = undefined;
-// const HEADLESS = true;
-
-// (async () => {
-//     const child = spawnSync('npm', ['run', 'build']);
-
-//     const browser = await puppeteer.launch({ headless: HEADLESS, executablePath: BROWSER_PATH});
-//     const page = await browser.newPage();
-//     await page.setViewport({
-//         width: 1280,
-//         height: 720,
-//         deviceScaleFactor: 1,
-//     });
-//     await page.goto('file://' + __dirname + '/assets/index.html');
-//     await page.screenshot({ path: path.join(__dirname, 'output', 'example.png') });
-
-//     await browser.close();
-// })();
 
 const timeout = 5000
 
--- tests/screenshot.test.js
+++ tests/screenshot.test.js
@@ -1,18 +1,3 @@
-// (async () => {
-//     const child = spawnSync('npm', ['run', 'build']);
-
-//     const browser = await puppeteer.launch({ headless: HEADLESS, executablePath: BROWSER_PATH});
-//     const page = await browser.newPage();
-//     await page.setViewport({
-//         width: 1280,
-//         height: 720,
-//         deviceScaleFactor: 1,
-//     });
-//     await page.goto('file://' + __dirname + '/assets/index.html');
-//     await page.screenshot({ path: path.join(__dirname, 'output', 'example.png') });
-
-//     await browser.close();
-// })();
 
 const path = require('path');
 const timeout = 5000
