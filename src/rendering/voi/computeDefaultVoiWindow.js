24a4925a4f6e826ffd64d33d4a5eace9ecfe6d95
Arne Groskurth <arne.groskurth@milchundzucker.de>
2021-12-20 21:49:56 +0100

    Code style

--- src/rendering/voi/computeDefaultVoiWindow.js
+++ src/rendering/voi/computeDefaultVoiWindow.js
@@ -10,7 +10,7 @@ export function computeDefaultVoiWindow(imageData) {
   const limits = imageData.getLimits();
 
   const center = (limits.min + limits.max) / 2;
-  const width = limits.max - limits.min
+  const width = limits.max - limits.min;
 
   return new VoiWindow(center, width);
 }
